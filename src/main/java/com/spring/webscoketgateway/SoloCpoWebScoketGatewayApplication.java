e5a912f74e0ea3e9db26a4b9fea1c92f8b673505
Imnotcoderdude <topsolo7@naver.com>
2026-02-25 10:47:51 +0900

    init commit

---
+++ src/main/java/com/spring/webscoketgateway/SoloCpoWebScoketGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.spring.webscoketgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoloCpoWebScoketGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoloCpoWebScoketGatewayApplication.class, args);
+    }
+
+}
